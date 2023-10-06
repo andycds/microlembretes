71f9d7784e5a9198f70fc34ca0480713aedff766
Anderson Sanches <andycds@gmail.com>
2023-10-06 19:12:56 -0300

    Início dos microserviços

---
+++ lembretes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const lembretes = {};
+contador = 0;
+
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes);
+});
+
+app.put('/lembretes', (req, res) => {
+    contador++;
+    const { texto } = req.body;
+    lembretes[contador] = {
+        contador, texto
+    }
+    res.status(201).send(lembretes[contador]);
+});
+
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000');
+});
