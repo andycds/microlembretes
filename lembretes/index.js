a5fec5da0e2ba897c3e7b3723fe56642472d255b
Anderson Sanches <andycds@gmail.com>
2023-11-24 21:39:37 -0300

    Mudanças para ler de um front-end simples

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require("axios");
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 const lembretes = {};
 contador = 0;
 
